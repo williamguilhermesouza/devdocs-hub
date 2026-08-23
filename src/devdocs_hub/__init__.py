dc9bbfa1dbd742fe04d969b4c12302a8b26ab07d
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-08-22 21:48:14 -0300

    initial commit

---
+++ src/devdocs_hub/__init__.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Hello from devdocs-hub!")
+
+def project_name() -> str:
+    return "DevDocs Hub"
---
+++ tests/test_smoke.py
@@ -0,0 +1,4 @@
+import devdocs_hub
+
+def test_smoke():
+    assert devdocs_hub.project_name() == "DevDocs Hub"
