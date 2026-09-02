55152cd17fe6334f1dc86860647858b9eb7d6c44
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-09-02 20:51:09 -0300

    adding worker and test worker

---
+++ src/devdocs_hub/ingestion/worker.py
@@ -0,0 +1,34 @@
+
+import asyncio
+from devdocs_hub.ingestion.fetcher import Fetcher
+
+
+class IngestionWorker:
+    def __init__(self, fetcher: Fetcher, maxsize: int = 0, r_maxsize: int = 0):
+        self.fetcher = fetcher
+        self.queue = asyncio.Queue[str](maxsize=maxsize)
+        self.r_queue = asyncio.Queue[str](maxsize=r_maxsize)
+
+    def start(self):
+        self.process_task = asyncio.create_task(self.process())
+
+    def submit(self, url: str) -> None:
+        self.queue.put_nowait(url)
+
+    async def process(self) -> None:
+        while (True):
+            next_urls = [await self.queue.get()]
+
+            while (True):
+                try:
+                    next_urls.append(self.queue.get_nowait())
+                except asyncio.QueueEmpty:
+                    break
+
+            responses = await self.fetcher.fetch_urls(next_urls)
+            for res in responses.values():
+                await self.r_queue.put(res)
+
+    def stop(self):
+        self.process_task.cancel()
+
---
+++ tests/ingestion/test_worker.py
@@ -0,0 +1,33 @@
+import asyncio
+from pytest import raises
+
+from devdocs_hub.ingestion.fetcher import Fetcher
+from devdocs_hub.ingestion.worker import IngestionWorker
+from tests.fakes.fake_client import FakeClient
+
+
+class TestIngestionWorker:
+    def test_process_urls(self):
+        asyncio.run(self._test_process_urls())
+
+    async def _test_process_urls(self):
+        client = FakeClient()
+        fetcher = Fetcher(client, max_parallel=10)
+        worker = IngestionWorker(fetcher, maxsize=10, r_maxsize=10)
+        worker.start()
+        url_list = [f'{i}url' for i in range(10)]
+
+        try:
+            for u in url_list:
+                worker.submit(u)
+
+            results = [
+                await asyncio.wait_for(worker.r_queue.get(), timeout=1)
+                for _ in url_list
+            ]
+
+            assert set(results) == set(url_list)
+        finally:
+            worker.stop()
+            with raises(asyncio.CancelledError):
+                await worker.process_task
