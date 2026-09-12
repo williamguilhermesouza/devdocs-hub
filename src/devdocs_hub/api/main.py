d24f017e3323d2fb3bef6cc435b02d3fb30c9cbf
williamguilhermesouza <williamguilhermesouza@gmail.com>
2026-09-12 10:40:41 -0300

    using sqlalchemy core instead of orm

--- src/devdocs_hub/api/main.py
+++ src/devdocs_hub/api/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 
-from devdocs_hub.db.core import emit_ddl
+from devdocs_hub.db.core import Database
+
+# from devdocs_hub.db.core import emit_ddl
 from .routers import documents
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-emit_ddl(engine)
+db = Database(engine)
+db.init()
+# emit_ddl(engine)
 
 app = FastAPI()
 app.include_router(documents.router)
--- src/devdocs_hub/db/core.py
+++ src/devdocs_hub/db/core.py
@@ -1,40 +1,94 @@
+from sqlalchemy import (
+    DateTime,
+    Integer,
+    MetaData,
+    String,
+    Table,
+    Column,
+    ForeignKey,
+    Engine,
+)
 from datetime import datetime, timezone
-from sqlalchemy import DateTime, Engine, ForeignKey, String
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy.orm.session import PROVISIONING_CONNECTION
 
 
-class Base(DeclarativeBase):
-    pass
+class Database:
+    def __init__(self, engine: Engine):
+        self._metadata_obj = MetaData()
+        self._engine = engine
+
+    def init(self):
+
+        self.users = Table(
+            "users",
+            self._metadata_obj,
+            Column("id", Integer, primary_key=True, autoincrement=True),
+            Column("username", String),
+        )
+
+        self.documents = Table(
+            "documents",
+            self._metadata_obj,
+            Column("id", Integer, primary_key=True, autoincrement=True),
+            Column("title", String, nullable=False),
+            Column("source", String),
+            Column("content", String),
+            Column(
+                "created_at",
+                DateTime(timezone=True),
+                default=lambda: datetime.now(timezone.utc),
+            ),
+        )
+        self.chunks = Table(
+            "chunks",
+            self._metadata_obj,
+            Column("id", Integer, primary_key=True, autoincrement=True),
+            Column("document_id", ForeignKey("documents.id")),
+            Column("position", Integer),
+            Column("content", String),
+            Column("embedding_id", Integer, nullable=True),
+        )
+
+        self._metadata_obj.create_all(self._engine)
 
 
-class User(Base):
-    __tablename__ = 'users'
-    id: Mapped[int] = mapped_column(
-        primary_key=True, autoincrement=True, nullable=False
-    )
-    username: Mapped[str] = mapped_column(String(30), nullable=False)
+# DON'T USE THAT YET, LEARN SQLALCHEMY CORE FIRST
+# from datetime import datetime, timezone
+# from sqlalchemy import DateTime, Engine, ForeignKey, String
+# from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+# from sqlalchemy.orm.session import PROVISIONING_CONNECTION
 
 
-class Document(Base):
-    __tablename__ = 'documents'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(nullable=False)
-    source: Mapped[str]
-    content: Mapped[str]
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
-    )
-
-
-class Chunk(Base):
-    __tablename__ = 'chunks'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    document_id = mapped_column(ForeignKey("documents.id"))
-    position: Mapped[int]
-    content: Mapped[str]
-    embedding_id = Mapped[int]
-
-
-def emit_ddl(engine: Engine):
-    Base.metadata.create_all(engine)
+# class Base(DeclarativeBase):
+#     pass
+#
+#
+# class User(Base):
+#     __tablename__ = 'users'
+#     id: Mapped[int] = mapped_column(
+#         primary_key=True, autoincrement=True, nullable=False
+#     )
+#     username: Mapped[str] = mapped_column(String(30), nullable=False)
+#
+#
+# class Document(Base):
+#     __tablename__ = 'documents'
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     title: Mapped[str] = mapped_column(nullable=False)
+#     source: Mapped[str]
+#     content: Mapped[str]
+#     created_at: Mapped[datetime] = mapped_column(
+#         DateTime(timezone=True), default=lambda: datetime.now(timezone.utc)
+#     )
+#
+#
+# class Chunk(Base):
+#     __tablename__ = 'chunks'
+#     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+#     document_id = mapped_column(ForeignKey("documents.id"))
+#     position: Mapped[int]
+#     content: Mapped[str]
+#     embedding_id = Mapped[int]
+#
+#
+# def emit_ddl(engine: Engine):
+#     Base.metadata.create_all(engine)
